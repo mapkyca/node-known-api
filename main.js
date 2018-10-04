2ed642bf504621d5a7b2cc39e48a528202b27065
Marcus Povey <marcus@marcus-povey.co.uk>
2018-10-04 15:12:30 +0100

    Posting a status message

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ var pjson = require('./package.json');
 var API = require("./API.js");
 var apicaller = new API(pjson.baseurl, pjson.username, pjson.apikey);
 
+
+apicaller.call(function(body){
+    console.log(body);
+}, '/status/edit', {
+    body: 'This is a test message'
+}, 'POST');
